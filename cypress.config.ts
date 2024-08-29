20cf9ef74d2d95c4100d33d95b97705d46922c99
Victor <innovimark@gmail.com>
2024-08-30 00:38:54 +0100

    added a test.yml file

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from "cypress";
 
 export default defineConfig({
   e2e: {
+    baseUrl: 'https://cxors.vercel.app',
     setupNodeEvents(on, config) {
       // implement node event listeners here
     },
